92e3209393eaee80375e7d997b9bad3f096c6687
Kelvin Bush <kelybush@gmail.com>
2022-08-30 14:19:39 +0300

    clean up

--- app/models/food.rb
+++
@@ -1,3 +0,0 @@
-class Food < ApplicationRecord
-  belongs_to :user, class_name: User, foreign_key: :user_id
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
-
-  has_many :foods, foreign_key: :user_id, dependent: delete_all
 end
