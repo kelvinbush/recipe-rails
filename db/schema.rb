bd98f32a694a0471497be954547dae6ac8a499d1
Kelvin Bush <kelybush@gmail.com>
2022-08-30 14:03:15 +0300

    clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_104156) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_100158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.decimal "price"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_foods_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -43,5 +32,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_104156) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "foods", "users"
 end
