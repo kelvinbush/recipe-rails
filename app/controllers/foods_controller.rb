4a9ec529563a23da3cd13b70c87726c07e3a8ea3
Kelvin Bush <kelybush@gmail.com>
2022-08-30 17:07:04 +0300

    set up foods controller actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,31 @@
 class FoodsController < ApplicationController
-  def index;end
+  def index
+    @user = User.first
+    @foods = Food.includes(:user).where(user:)
+  end
+
+  def create
+    @user = User.first
+    @food = @user.foods.create(food_params)
+    if @food.save
+      flash[:notice] = 'New Food Created Successfully.'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Food creation failed'
+      render action: 'index'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    render action: 'index'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
 end
