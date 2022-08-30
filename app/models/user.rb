72c1cf505d4709a42b4e218e0b135fe26a69016a
Kelvin Bush <kelybush@gmail.com>
2022-08-30 14:29:15 +0300

    implement food, user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  has_many :foods, foreign_key: :user_id, dependent: :delete_all
 end
