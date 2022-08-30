b7e026af8c3cdd68df54e0cc5ef678a4d0dcee14
Kelvin Bush <kelybush@gmail.com>
2022-08-30 18:18:32 +0300

    set up foods routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'foods#index'
-  resources :foods, only: [:index, :create, :destroy]
+  resources :foods, only: [:index, :create, :new, :destroy]
 end
