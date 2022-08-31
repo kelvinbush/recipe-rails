61206fabae79f85dde84e80e25b539f2a2526d98
Kelvin Bush <kelybush@gmail.com>
2022-08-31 16:36:05 +0300

    remove confirm email functionality

--- Gemfile
+++ Gemfile
@@ -65,7 +65,6 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
-  gem "letter_opener", "~> 1.8"
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :foods, foreign_key: :user_id, dependent: :delete_all
 end
