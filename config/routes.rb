e7bbfdcc5c416db81853e5c6c2ff8cf3a4818b71
camilevahviraki <camilevahviraki@gmail.com>
2022-08-31 11:32:07 +0200

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'foods#index'
+  resources :recipes, only: %i[index new show create destroy update edit] do
+    resources :recipe_foods, only: %i[new create destroy]
+  end  
+
 end
